06f61c1e9150dbc23e1afd55b8abb6f4ad31c195
Anant Gupta <anant718@gmail.com>
2014-12-16 15:05:07 -0500

    url changes

--- Ryde/urls.py
+++ Ryde/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Ryde.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url (r'^v0/', include('v0.urls')),
+    # url(r'^admin/', include(admin.site.urls)),
 )
