34a0dc9f74f74e619880eeccadda4866e8c98518
Anant Gupta <anant718@gmail.com>
2015-01-18 04:44:31 -0500

    map settings

--- v0/views.py
+++ v0/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.template import RequestContext, loader
 from django.http import HttpResponse
 from models import *
+from django.utils import timezone
 # Create your views here.
 def home(request):
-	return HttpResponse('<h1>ok!</h1')
+	get_ride_link = '<h1><a href="book">Get me a ride</a></h1>'
+	context = {
+		'link' : get_ride_link,
+	}
+	#return HttpResponse(template.render(context))	
+	return render(request, 'v0/home.html', context)
+
+def book(request):
+	start_ride = Rides(last_started, long_started, time_started = timezone.now())
+ 		
