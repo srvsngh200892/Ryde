9f9c2a6154f74f552f404be447c9beb8bb3d6aff
Anant Gupta <anant718@gmail.com>
2015-01-26 07:36:46 -0500

    basic modules

--- Ryde/urls.py
+++ Ryde/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'Ryde.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+     #url(r'^$', 'Ryde.views.home', name='home'),
+    #url(r'^blog/', include('blog.urls')),
+    #url (r'^$', include('v0.urls')),
     url (r'^v0/', include('v0.urls')),
-    # url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^admin/', include(admin.site.urls)),
+) 
+
+urlpatterns += staticfiles_urlpatterns()
--- v0/views.py
+++ v0/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
+from django.template import RequestContext, loader
 from django.http import HttpResponse
 from models import *
+from django.utils import timezone
+from django.db import connection
 # Create your views here.
 def home(request):
-	return HttpResponse('<h1>ok!</h1')
+	get_ride_link = '<h1><a href="book">Get me a ride</a></h1>'
+	context = {
+		'link' : get_ride_link,
+	}
+	#return HttpResponse(template.render(context))	
+	return render(request, 'v0/home.html', context)
+
+def book(request):
+	start_ride = Rides(last_started, long_started, time_started = timezone.now())
+
+
+'''
+@param current corrds, destination
+@return matching rides
+'''
+def getLocation(request):
+	#check validity of coords
+	#log action
+	#get nearby taxis
+	rides = getNearbyRides(request['lat'], request['long'])
+	if len(rides) > 0:
+	data = { 'rides' : rides }
+	return render( request, 'rides', data)
+	
+def getNearbyRides(lat, lng):
+	#v1 go with SQL
+	query = "SELECT * FROM ride_locations WHERE "
+	cursor = connection.cursor()
+	result = cursor.execute(query)
+
+
+
+	 		
